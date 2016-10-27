287521f94195e4ea551422a616d7cd631d57e07e
Dung Kieu Tien <ktdunglibre@gmail.com>
2016-10-28 04:30:24 +0700

    add chapter 4 assignment

--- javascript/learn_js/ex_chapter5/c5_demo1.js
+++ javascript/learn_js/ex_chapter5/c5_demo1.js
@@ -18,11 +18,39 @@ logEach(array, console.log);
 
 // c3: toi uu c2
 
-var numbers = [1, 2, 3, 4, 5], sum = 0;
-forEach(numbers, function(number) {
-    sum += number;
-});
-console.log(sum);
-// → 15
+//var numbers = [1, 2, 3, 4, 5], sum = 0;
+//forEach(numbers, function(number) {
+//    sum += number;
+//});
+//console.log(sum);
+//// → 15
 
 // HIGHER-ORDER FUNCTIONS
+function graterThan(n) {
+    return function (m) { return m > n;};
+}
+
+var greaterThan10 = graterThan(10);
+console.log(greaterThan10(11));
+
+function noisy(f) {
+    return function (arg) {
+        console.log("calling with", arg);
+        var val = f(arg);
+        console.log("called with", arg, "- got", val);
+        return val;
+    };
+}
+noisy(Boolean)(0);
+
+function unless(test, then) {
+    if (!test) then();
+}
+function repeat(times, body) {
+    for (var i = 0; i < times; i++) body(i);
+}
+repeat(3, function (n) {
+    unless(n % 2, function () {
+        console.log(n, "is even");
+    });
+});
